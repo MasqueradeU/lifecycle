740f8afc668b56e7138f9c37d7dad1d8853f3815
Hitomi Hatsukaze <gigigiaru776@yahoo.co.jp>
2018-11-15 00:24:21 +0900

    lifecycle hook between beforeCreated and updated

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app" :style="appStyle">
     <img src="./assets/logo.png">
+    <br>
+    <button @click="extendWidth">extend</button>
     <app-child/>
   </div>
 </template>
@@ -24,24 +26,48 @@ export default {
     }
   },
 
+  methods: {
+    extendWidth () {
+      this.appStyle.width = parseInt(this.appStyle.width) + 10 + '%'
+    }
+  },
+
   // Reactive data has not been initialized yet. This section is just before it.
   beforeCreate () {
+    console.log('beforeCreate')
     console.log(this.name) // Output 'undefined'
   },
 
   // Reactive data was initialized.
   created () {
+    console.log('create')
     console.log(this.name) // Output 'hatsukaze hitomi'
   },
 
   // DOM has not been structured yet. This section is just before it.
   beforeMount () {
+    console.log('beforeMount')
     console.log(document.getElementById('app').clientWidth) // Output '785' (It depends on the environment)
   },
 
   // DOM was structured.
   mounted () {
+    console.log('mounted')
     console.log(document.getElementById('app').clientWidth) // Output '942' (It depends on the environment)
+  },
+
+  beforeUpdate () {
+    console.log('beforeUpdate')
+    // Output '942' (It depends on the environment) if button was clicked once.
+    // Output '1021' (It depends on the environment) if button was clicked twice.
+    console.log(document.getElementById('app').clientWidth)
+  },
+
+  updated () {
+    console.log('updated')
+    // Output '1021' (It depends on the environment) if button was clicked once.
+    // Output '1099' (It depends on the environment) if button was clicked twice.
+    console.log(document.getElementById('app').clientWidth)
   }
 }
 </script>
